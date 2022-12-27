e62f7cfe63eb45f298e4d1ac3446ab2ca8da2394
yahiaehab10 <102036052+yahiaehab10@users.noreply.github.com>
2022-12-27 18:36:51 +0200

    Lecturer Page 2

--- webpages/LecturerPage.aspx
+++ webpages/LecturerPage.aspx
@@ -9,7 +9,197 @@
 <body>
     <form id="form1" runat="server">
         <div>
+            <asp:Label ID="Label1" runat="server" Text="Lecturer Page"></asp:Label>
+            <br />
+            <br />
         </div>
+        <asp:Label ID="Label2" runat="server" Text="Create local project"></asp:Label>
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Create Project" />
+        <p>
+            <asp:Label ID="Label3" runat="server" Text="Lecturer ID"></asp:Label>
+            <asp:TextBox ID="TextBox1" runat="server" Width="173px"></asp:TextBox>
+        </p>
+        <p>
+            <asp:Label ID="Label4" runat="server" Text="Project code"></asp:Label>
+            <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+            <asp:Label ID="Label5" runat="server" Text="Major Code"></asp:Label>
+            <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+        </p>
+        <asp:Label ID="Label6" runat="server" Text="Title"></asp:Label>
+        <asp:TextBox ID="TextBox4" runat="server"></asp:TextBox>
+        <asp:Label ID="Label7" runat="server" Text="Description"></asp:Label>
+        <asp:TextBox ID="TextBox5" runat="server"></asp:TextBox>
+        <p>
+            &nbsp;</p>
+        <p>
+            &nbsp;</p>
+        <p>
+            Specify deadline<asp:Button ID="Button2" runat="server" Text="Submit" OnClick="Button2_Click" />
+        </p>
+        Deadline<asp:TextBox ID="TextBox6" runat="server"></asp:TextBox>
+        <p>
+            &nbsp;</p>
+        <asp:Label ID="Label8" runat="server" Text="Create meetings"></asp:Label>
+        <asp:Button ID="Button3" runat="server" OnClick="Button3_Click" Text="Submit" Height="31px" Width="70px" />
+        <p>
+            <asp:Label ID="Label9" runat="server" Text="Lecturer Id"></asp:Label>
+        <asp:TextBox ID="TextBox7" runat="server"></asp:TextBox>
+            <asp:Label ID="Label10" runat="server" Text="Start Time"></asp:Label>
+            <asp:TextBox ID="TextBox8" runat="server"></asp:TextBox>
+        </p>
+        <asp:Label ID="Label11" runat="server" Text="End time"></asp:Label>
+        <asp:TextBox ID="TextBox9" runat="server"></asp:TextBox>
+        <asp:Label ID="Label12" runat="server" Text="Date"></asp:Label>
+        <asp:TextBox ID="TextBox10" runat="server"></asp:TextBox>
+        <p>
+            <asp:Label ID="Label13" runat="server" Text="Meeting point"></asp:Label>
+            <asp:TextBox ID="TextBox11" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            &nbsp;</p>
+        <p>
+            &nbsp;</p>
+        <asp:Label ID="Label14" runat="server" Text="Add to the to-do "></asp:Label>
+        <asp:Button ID="Button4" runat="server" Text="Submit" OnClick="Button4_Click" />
+        <br />
+        <br />
+        <asp:Label ID="Label15" runat="server" Text="Meeting ID"></asp:Label>
+        <asp:TextBox ID="TextBox12" runat="server"></asp:TextBox>
+        &nbsp;<asp:Label ID="Label16" runat="server" Text="To do list"></asp:Label>
+        <asp:TextBox ID="TextBox13" runat="server"></asp:TextBox>
+        <p>
+            &nbsp;</p>
+        <p>
+            &nbsp;</p>
+        <asp:Label ID="Label17" runat="server" Text="View Meeting"></asp:Label>
+        &nbsp;<asp:Button ID="Button5" runat="server" Text="view" OnClick="Button5_Click" />
+        <p>
+            <asp:Label ID="Label18" runat="server" Text="Lecturer ID"></asp:Label>
+            <asp:TextBox ID="TextBox14" runat="server"></asp:TextBox>
+            &nbsp;<asp:Label ID="Label19" runat="server" Text="Meeting ID"></asp:Label>
+            <asp:TextBox ID="TextBox15" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            &nbsp;</p>
+        <p>
+            &nbsp;</p>
+        <p>
+        <asp:Label ID="Label20" runat="server" Text="View external examiners"></asp:Label>
+        <asp:Button ID="Button6" runat="server" Text="view" OnClick="Button5_Click" />
+        </p>
+        <p>
+            &nbsp;</p>
+        <p>
+            &nbsp;</p>
+        <p>
+            <asp:Label ID="Label44" runat="server" Text="Lecturer recommend external examiner"></asp:Label>
+            <asp:Button ID="Button12" runat="server" Text="Recommend" />
+        </p>
+        <p>
+            <asp:Label ID="Label45" runat="server" Text="Lecture ID"></asp:Label>
+            <asp:TextBox ID="TextBox35" runat="server"></asp:TextBox>
+&nbsp;
+            <asp:Label ID="Label46" runat="server" Text="Project code"></asp:Label>
+            <asp:TextBox ID="TextBox36" runat="server"></asp:TextBox>
+        &nbsp;
+            <asp:Label ID="Label47" runat="server" Text="External examiner ID"></asp:Label>
+            <asp:TextBox ID="TextBox37" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            &nbsp;</p>
+        <p>
+            &nbsp;</p>
+        <p>
+            <asp:Label ID="Label21" runat="server" Text="Supervise Industrial"></asp:Label>
+        <asp:Button ID="Button7" runat="server" Text="Supervise" OnClick="Button5_Click" />
+        </p>
+        <p>
+            <asp:Label ID="Label22" runat="server" Text="Lecturer ID"></asp:Label>
+            <asp:TextBox ID="TextBox17" runat="server"></asp:TextBox>
+            <asp:Label ID="Label23" runat="server" Text="Project code"></asp:Label>
+            <asp:TextBox ID="TextBox18" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            &nbsp;</p>
+        <p>
+            &nbsp;</p>
+        <p>
+            <asp:Label ID="Label24" runat="server" Text="Grade Thesis"></asp:Label>
+        <asp:Button ID="Button8" runat="server" Text="Grade" OnClick="Button5_Click" />
+        </p>
+        <p>
+            <asp:Label ID="Label25" runat="server" Text="Lecturer ID"></asp:Label>
+            <asp:TextBox ID="TextBox19" runat="server"></asp:TextBox>
+            <asp:Label ID="Label26" runat="server" Text="Student ID"></asp:Label>
+            <asp:TextBox ID="TextBox20" runat="server" OnTextChanged="TextBox20_TextChanged"></asp:TextBox>
+        </p>
+        <p>
+            <asp:Label ID="Label27" runat="server" Text="Thesis title"></asp:Label>
+            <asp:TextBox ID="TextBox21" runat="server"></asp:TextBox>
+            <asp:Label ID="Label28" runat="server" Text="Lecturer grade"></asp:Label>
+            <asp:TextBox ID="TextBox22" runat="server" OnTextChanged="TextBox20_TextChanged"></asp:TextBox>
+        </p>
+        <p>
+            &nbsp;</p>
+        <p>
+            &nbsp;</p>
+        <p>
+            <asp:Label ID="Label29" runat="server" Text="Grade Defense"></asp:Label>
+        <asp:Button ID="Button9" runat="server" Text="Grade" OnClick="Button5_Click" />
+        </p>
+        <p>
+            <asp:Label ID="Label30" runat="server" Text="Lecturer ID"></asp:Label>
+            <asp:TextBox ID="TextBox23" runat="server"></asp:TextBox>
+            <asp:Label ID="Label31" runat="server" Text="Student ID"></asp:Label>
+            <asp:TextBox ID="TextBox24" runat="server" OnTextChanged="TextBox20_TextChanged"></asp:TextBox>
+        </p>
+        <p>
+            <asp:Label ID="Label32" runat="server" Text="Defense location"></asp:Label>
+            <asp:TextBox ID="TextBox25" runat="server"></asp:TextBox>
+            <asp:Label ID="Label33" runat="server" Text="Lecturer grade"></asp:Label>
+            <asp:TextBox ID="TextBox26" runat="server" OnTextChanged="TextBox20_TextChanged"></asp:TextBox>
+        </p>
+        <p>
+            &nbsp;</p>
+        <p>
+            &nbsp;</p>
+        <p>
+            <asp:Label ID="Label34" runat="server" Text="Create progress report"></asp:Label>
+            <asp:Button ID="Button10" runat="server" Text="Create" OnClick="Button10_Click" />
+        </p>
+        <p>
+            <asp:Label ID="Label35" runat="server" Text="Lecturer ID"></asp:Label>
+            <asp:TextBox ID="TextBox27" runat="server"></asp:TextBox>
+            <asp:Label ID="Label36" runat="server" Text="Student ID"></asp:Label>
+            <asp:TextBox ID="TextBox28" runat="server" OnTextChanged="TextBox20_TextChanged"></asp:TextBox>
+        </p>
+        <p>
+            <asp:Label ID="Label37" runat="server" Text="Date"></asp:Label>
+            <asp:TextBox ID="TextBox29" runat="server"></asp:TextBox>
+            <asp:Label ID="Label38" runat="server" Text="Conent"></asp:Label>
+            <asp:TextBox ID="TextBox30" runat="server" OnTextChanged="TextBox20_TextChanged"></asp:TextBox>
+        </p>
+        <p>
+            &nbsp;</p>
+        <p>
+            &nbsp;</p>
+        <p>
+            <asp:Label ID="Label39" runat="server" Text="Grade Progress Report"></asp:Label>
+            <asp:Button ID="Button11" runat="server" Text="Grade" OnClick="Button11_Click" />
+        </p>
+        <p>
+            <asp:Label ID="Label40" runat="server" Text="Lecturer ID"></asp:Label>
+            <asp:TextBox ID="TextBox31" runat="server"></asp:TextBox>
+            <asp:Label ID="Label41" runat="server" Text="Student ID"></asp:Label>
+            <asp:TextBox ID="TextBox32" runat="server" OnTextChanged="TextBox20_TextChanged"></asp:TextBox>
+        </p>
+        <p>
+            <asp:Label ID="Label42" runat="server" Text="Date"></asp:Label>
+            <asp:TextBox ID="TextBox33" runat="server"></asp:TextBox>
+            <asp:Label ID="Label43" runat="server" Text="Lecturer grade"></asp:Label>
+            <asp:TextBox ID="TextBox34" runat="server" OnTextChanged="TextBox20_TextChanged"></asp:TextBox>
+        </p>
     </form>
 </body>
 </html>
+
--- webpages/LecturerPage.aspx.cs
+++ webpages/LecturerPage.aspx.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Web;
+using System.Web.Configuration;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
@@ -9,9 +13,234 @@ namespace WebApplication3.webpages
 {
     public partial class LecturerPage : System.Web.UI.Page
     {
+        SqlConnection conn = new SqlConnection(WebConfigurationManager.ConnectionStrings["Portal"].ToString());
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (Session["user"] == null)
+            {
+                Response.Redirect("LoginPage.aspx");
+            }
 
+        }
+
+        protected void ShowCreateProject(object sender, EventArgs e)
+        {
+            if (!Label1.Visible)
+            {
+                Label1.Visible = true;
+            }
+            else
+            {
+                Label1.Visible = false;
+            }
+        }
+
+        protected void CreateProjectClicked(object sender, EventArgs e)
+        {
+            conn.Open();
+            SqlCommand CreateProject = new SqlCommand("LecturerCreateLocalProject", conn);
+            CreateProject.CommandType = CommandType.StoredProcedure;
+            SqlParameter userIdEntered = CreateProject.Parameters.Add(new SqlParameter("@lecturer_id", SqlDbType.Int));
+            userIdEntered.Value = Int32.Parse(Request.QueryString["UserID"]);
+            SqlParameter title = CreateProject.Parameters.Add(new SqlParameter("@title", SqlDbType.VarChar, 50));
+            title.Value = Label6.Text;
+            SqlParameter description = CreateProject.Parameters.Add(new SqlParameter("@description", SqlDbType.VarChar, 200));
+            description.Value = Label7.Text;
+            SqlParameter majorcode = CreateProject.Parameters.Add(new SqlParameter("@major_code", SqlDbType.VarChar, 10));
+            majorcode.Value = Label5.Text;
+
+            SqlParameter projCode = CreateProject.Parameters.Add(new SqlParameter("@proj_code", SqlDbType.Int));
+            projCode.Value = Label4.Text;
+
+            CreateProject.ExecuteNonQuery();
+            conn.Close();
+
+        }
+
+        protected void SpecifyThesisDeadline(object sender, EventArgs e)
+        {
+            conn.Open();
+            SqlCommand CreateProject = new SqlCommand("SpecifyThesisDeadline", conn);
+            CreateProject.CommandType = CommandType.StoredProcedure;
+            SqlParameter userIdEntered = CreateProject.Parameters.Add(new SqlParameter("@lecturer_id", SqlDbType.Int));
+            userIdEntered.Value = Int32.Parse(Request.QueryString["UserID"]);
+
+            CreateProject.ExecuteNonQuery();
+            conn.Close();
+        }
+
+        protected void CreateMeeting(object sender, EventArgs e)
+        {
+            conn.Open();
+            SqlCommand CreateProject = new SqlCommand("CreateMeeting", conn);
+            CreateProject.CommandType = CommandType.StoredProcedure;
+            SqlParameter userIdEntered = CreateProject.Parameters.Add(new SqlParameter("@lecturer_id", SqlDbType.Int));
+            userIdEntered.Value = Int32.Parse(Request.QueryString["UserID"]);
+            SqlParameter startTime = CreateProject.Parameters.Add(new SqlParameter("@start_time", SqlDbType.VarChar, 50));
+            startTime.Value = Label10.Text;
+            SqlParameter endTime = CreateProject.Parameters.Add(new SqlParameter("@end_time", SqlDbType.VarChar, 200));
+            endTime.Value = Label11.Text;
+            SqlParameter date = CreateProject.Parameters.Add(new SqlParameter("@date", SqlDbType.VarChar, 10));
+            date.Value = Label12.Text;
+            SqlParameter meetingPoint = CreateProject.Parameters.Add(new SqlParameter("@meeting_point", SqlDbType.Int));
+            meetingPoint.Value = Label13.Text;
+
+            CreateProject.ExecuteNonQuery();
+            conn.Close();
+        }
+
+        protected void LecturerAddToDo(object sender, EventArgs e)
+        {
+            conn.Open();
+            SqlCommand CreateProject = new SqlCommand("LecturerAddToDo", conn);
+            CreateProject.CommandType = CommandType.StoredProcedure;
+            SqlParameter userIdEntered = CreateProject.Parameters.Add(new SqlParameter("@meeting_id", SqlDbType.Int));
+            userIdEntered.Value = Int32.Parse(Request.QueryString["UserID"]);
+            SqlParameter toDoList = CreateProject.Parameters.Add(new SqlParameter("@to_do_list", SqlDbType.VarChar, 50));
+            toDoList.Value = Label16.Text;
+
+            CreateProject.ExecuteNonQuery();
+            conn.Close();
+        }
+
+        protected void ViewMeetingLecturer(object sender, EventArgs e)
+        {
+            conn.Open();
+            SqlCommand CreateProject = new SqlCommand("ViewMeetingLecturer", conn);
+            CreateProject.CommandType = CommandType.StoredProcedure;
+            SqlParameter userIdEntered = CreateProject.Parameters.Add(new SqlParameter("@lecturer_id", SqlDbType.Int));
+            userIdEntered.Value = Int32.Parse(Request.QueryString["lecturer_id"]);
+            SqlParameter meeting = CreateProject.Parameters.Add(new SqlParameter("@meeting_id", SqlDbType.VarChar, 50));
+            meeting.Value = Int32.Parse(Request.QueryString["meeting_id"]);
+
+            CreateProject.ExecuteNonQuery();
+            conn.Close();
+        }
+
+        protected void viewEE(object sender, EventArgs e)
+        {
+            conn.Open();
+            SqlCommand CreateProject = new SqlCommand("ViewEE", conn);
+            CreateProject.CommandType = CommandType.StoredProcedure;
+
+            CreateProject.ExecuteNonQuery();
+            conn.Close();
+        }
+
+        protected void SuperviseIndustrial(object sender, EventArgs e)
+        {
+            conn.Open();
+            SqlCommand CreateProject = new SqlCommand("SuperviseIndustrial", conn);
+            CreateProject.CommandType = CommandType.StoredProcedure;
+            SqlParameter userIdEntered = CreateProject.Parameters.Add(new SqlParameter("@lecturer_id", SqlDbType.Int));
+            userIdEntered.Value = Int32.Parse(Request.QueryString["lecturer_id"]);
+            SqlParameter studentId = CreateProject.Parameters.Add(new SqlParameter("@proj_Code", SqlDbType.VarChar, 50));
+            studentId.Value = Int32.Parse(Request.QueryString["proj_Code"]);
+
+            CreateProject.ExecuteNonQuery();
+            conn.Close();
+        }
+
+        protected void LecGradeThesis(object sender, EventArgs e)
+        {
+            conn.Open();
+            SqlCommand CreateProject = new SqlCommand("LecGradeThesis", conn);
+            CreateProject.CommandType = CommandType.StoredProcedure;
+            SqlParameter userIdEntered = CreateProject.Parameters.Add(new SqlParameter("@lecturer_id", SqlDbType.Int));
+            userIdEntered.Value = Int32.Parse(Request.QueryString["lecturer_id"]);
+            SqlParameter meeting = CreateProject.Parameters.Add(new SqlParameter("@sid", SqlDbType.VarChar, 50));
+            meeting.Value = Int32.Parse(Request.QueryString["sid"]);
+            SqlParameter thesis_title = CreateProject.Parameters.Add(new SqlParameter("@thesis_title", SqlDbType.VarChar, 50));
+            thesis_title.Value = Label27.Text;
+            SqlParameter Lecturer_grade = CreateProject.Parameters.Add(new SqlParameter("@Lecturer_grade", SqlDbType.VarChar, 200));
+            Lecturer_grade.Value = Label28.Text;
+
+
+            CreateProject.ExecuteNonQuery();
+            conn.Close();
+        }
+
+        protected void LecGradedefense(object sender, EventArgs e)
+        {
+            conn.Open();
+            SqlCommand CreateProject = new SqlCommand("LecGradedefense", conn);
+            CreateProject.CommandType = CommandType.StoredProcedure;
+            SqlParameter userIdEntered = CreateProject.Parameters.Add(new SqlParameter("@lecturer_id", SqlDbType.Int));
+            userIdEntered.Value = Int32.Parse(Request.QueryString["lecturer_id"]);
+            SqlParameter meeting = CreateProject.Parameters.Add(new SqlParameter("@sid", SqlDbType.VarChar, 50));
+            meeting.Value = Int32.Parse(Request.QueryString["sid"]);
+            SqlParameter defense_location = CreateProject.Parameters.Add(new SqlParameter("@defense_location", SqlDbType.VarChar, 50));
+            defense_location.Value = Label32.Text;
+            SqlParameter Lecturer_grade = CreateProject.Parameters.Add(new SqlParameter("@Lecturer_grade", SqlDbType.VarChar, 200));
+            Lecturer_grade.Value = Label33.Text;
+
+
+            CreateProject.ExecuteNonQuery();
+            conn.Close();
+        }
+
+        protected void LecCreatePR(object sender, EventArgs e)
+        {
+            conn.Open();
+            SqlCommand CreateProject = new SqlCommand("LecCreatePR", conn);
+            CreateProject.CommandType = CommandType.StoredProcedure;
+            SqlParameter userIdEntered = CreateProject.Parameters.Add(new SqlParameter("@lecturer_id", SqlDbType.Int));
+            userIdEntered.Value = Int32.Parse(Request.QueryString["lecturer_id"]);
+            SqlParameter meeting = CreateProject.Parameters.Add(new SqlParameter("@sid", SqlDbType.VarChar, 50));
+            meeting.Value = Int32.Parse(Request.QueryString["sid"]);
+            SqlParameter Date = CreateProject.Parameters.Add(new SqlParameter("@date", SqlDbType.VarChar, 50));
+            Date.Value = Label37.Text;
+            SqlParameter Content = CreateProject.Parameters.Add(new SqlParameter("@content", SqlDbType.VarChar, 200));
+            Content.Value = Label38.Text;
+
+            CreateProject.ExecuteNonQuery();
+            conn.Close();
+        }
+
+        protected void LecGradePR(object sender, EventArgs e)
+        {
+            conn.Open();
+            SqlCommand CreateProject = new SqlCommand("LecGradePR", conn);
+            CreateProject.CommandType = CommandType.StoredProcedure;
+            SqlParameter userIdEntered = CreateProject.Parameters.Add(new SqlParameter("@lecturer_id", SqlDbType.Int));
+            userIdEntered.Value = Int32.Parse(Request.QueryString["lecturer_id"]);
+            SqlParameter meeting = CreateProject.Parameters.Add(new SqlParameter("@sid", SqlDbType.VarChar, 50));
+            meeting.Value = Int32.Parse(Request.QueryString["sid"]);
+            SqlParameter Date = CreateProject.Parameters.Add(new SqlParameter("@date", SqlDbType.VarChar, 50));
+            Date.Value = Label42.Text;
+            SqlParameter lecturerGrade = CreateProject.Parameters.Add(new SqlParameter("@lecturer_grade", SqlDbType.VarChar, 200));
+            lecturerGrade.Value = Label43.Text;
+
+            CreateProject.ExecuteNonQuery();
+            conn.Close();
+        }
+
+        protected void RecommendEE(object sender, EventArgs e)
+        {
+            conn.Open();
+            SqlCommand CreateProject = new SqlCommand("RecommendEE", conn);
+            CreateProject.CommandType = CommandType.StoredProcedure;
+            SqlParameter userIdEntered = CreateProject.Parameters.Add(new SqlParameter("@lecturer_id", SqlDbType.Int));
+            userIdEntered.Value = Int32.Parse(Request.QueryString["lecturer_id"]);
+            SqlParameter projCode = CreateProject.Parameters.Add(new SqlParameter("@proj_code", SqlDbType.VarChar, 50));
+            projCode.Value = Int32.Parse(Request.QueryString["proj_code"]);
+            SqlParameter externalExaminer = CreateProject.Parameters.Add(new SqlParameter("@EE_id", SqlDbType.VarChar, 50));
+            externalExaminer.Value = Int32.Parse(Request.QueryString["EE_id"]);
+
+
+            CreateProject.ExecuteNonQuery();
+            conn.Close();
+        }
+
+        protected void ViewEE(object sender, EventArgs e)
+        {
+            conn.Open();
+            SqlCommand CreateProject = new SqlCommand("ViewEE", conn);
+            CreateProject.CommandType = CommandType.StoredProcedure;
+
+
+            CreateProject.ExecuteNonQuery();
+            conn.Close();
         }
     }
 }
